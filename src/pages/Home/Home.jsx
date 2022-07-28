565437092f6914f2ba94f7a1c3658ddc4c15e80f
Shoaib Afzaal <100206484+ShibCode@users.noreply.github.com>
2022-07-28 19:43:03 +0500

    commit

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { useState } from "react";
 
 import { Link } from "react-router-dom";
 
@@ -9,18 +10,22 @@ import Restaurants from "../../assets/Restaurants.png";
 import "./Home.css";
 
 const Home = () => {
+  const [email, setEmail] = useState("");
+
   return (
     <div className="home">
-      <div className="home__headerContent">
-        <Link to="/" className="home__logo">
-          Travel Companion
-        </Link>
-
-        <div className="home__links">
-          <Link to="/explore" className="home__link">
-            Explore
+      <div className="home__header">
+        <div className="home__headerContent">
+          <Link to="/" className="home__logo">
+            Travel Companion
           </Link>
-          <div className="home__link">Contact Us</div>
+
+          <div className="home__links">
+            <Link to="/explore" className="home__link">
+              Explore
+            </Link>
+            <div className="home__link">Contact Us</div>
+          </div>
         </div>
       </div>
 
@@ -47,6 +52,45 @@ const Home = () => {
           <div>
             <img src={Attractions} alt="attractions" />
           </div>
+        </div>
+      </div>
+
+      <div className="home__footer">
+        <div className="home__footerTop">
+          <div>
+            Want to recieve exclusive hotel offers? Subscribe to our newsletter!
+          </div>
+
+          <form
+            className="home__footerTop-right"
+            onSubmit={(e) => {
+              e.preventDefault();
+              setEmail("");
+            }}
+          >
+            <input
+              type="text"
+              placeholder="Email address"
+              className="home__emailInput"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            <button type="submit" className="home__subscribe">
+              Subscribe
+            </button>
+          </form>
+        </div>
+
+        <div className="home__footerBottom">
+          <div className="home__footerBottom-top">
+            <div className="home__footerBottom-topLeft">
+              <span className="home__footerUpperText"></span>
+            </div>
+          </div>
+
+          <div></div>
+
+          <div></div>
         </div>
       </div>
     </div>
