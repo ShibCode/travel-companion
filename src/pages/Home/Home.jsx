aba8bea61ceed7e0010512bcc03efb84a9cdfc49
sikander-afzaal <72333576+sikander-afzaal@users.noreply.github.com>
2022-07-28 20:44:23 +0500

    commit

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,6 +1,16 @@
 import React from "react";
 import { useState } from "react";
 
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+import {
+  faFacebookF,
+  faTwitter,
+  faInstagram,
+  faLinkedinIn,
+  faYoutube,
+} from "@fortawesome/free-brands-svg-icons";
+
 import { Link } from "react-router-dom";
 
 import Attractions from "../../assets/Attractions.png";
@@ -84,13 +94,86 @@ const Home = () => {
         <div className="home__footerBottom">
           <div className="home__footerBottom-top">
             <div className="home__footerBottom-topLeft">
-              <span className="home__footerUpperText"></span>
+              <div className="home__footerUpperText">
+                <span>Travelling Companion.</span> Lorem, ipsum dolor,
+              </div>
+
+              <div className="home__footerLowerText">Germany</div>
+            </div>
+
+            <select id="country">
+              <option value="USA">USA</option>
+              <option value="UK">UK</option>
+              <option value="Cyprus">Cyprus</option>
+              <option value="Kosovo">Kosovo</option>
+              <option value="Poland">Poland</option>
+            </select>
+          </div>
+
+          <div className="home__footerBottom-center1">
+            <div className="home__footerSocial">
+              <FontAwesomeIcon icon={faFacebookF} />
+            </div>
+
+            <div className="home__footerSocial">
+              <FontAwesomeIcon icon={faTwitter} />
+            </div>
+
+            <div className="home__footerSocial">
+              <FontAwesomeIcon icon={faInstagram} />
+            </div>
+
+            <div className="home__footerSocial">
+              <FontAwesomeIcon icon={faYoutube} />
+            </div>
+
+            <div className="home__footerSocial">
+              <FontAwesomeIcon icon={faLinkedinIn} />
             </div>
           </div>
 
-          <div></div>
+          <div className="home__footerBottom-center2">
+            <ul className="home__footerList">
+              <li className="home__footerListItem">Company</li>
+              <li className="home__footerListItem">Jobs</li>
+              <li className="home__footerListItem">Press</li>
+              <li className="home__footerListItem">Investor relations</li>
+            </ul>
 
-          <div></div>
+            <ul className="home__footerList">
+              <li className="home__footerListItem">
+                Mobile apps - searching on the go
+              </li>
+              <li className="home__footerListItem">
+                Travelling Companion Business Studio
+              </li>
+            </ul>
+
+            <ul className="home__footerList">
+              <li className="home__footerListItem">Help</li>
+              <li className="home__footerListItem">
+                Learn how travelling companion works
+              </li>
+              <li className="home__footerListItem">Terms and conditions</li>
+              <li className="home__footerListItem">Legal information</li>
+              <li className="home__footerListItem">
+                Do not sell my personal information
+              </li>
+              <li className="home__footerListItem">Privacy notice</li>
+              <li className="home__footerListItem">Cyber security</li>
+              <li className="home__footerListItem">
+                Coronavirus (Covid-19) travel advice
+              </li>
+            </ul>
+          </div>
+
+          <div className="home__footerBottom-bottom">
+            <div className="home__footerLogo">Travelling Companion</div>
+
+            <div className="home__footerCopyright">
+              Copyright 2022 Travelling Companion | All rights reserved
+            </div>
+          </div>
         </div>
       </div>
     </div>
