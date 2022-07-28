ceab4652e4d48875b4afdef23c6fbf607cd5b481
Shoaib Afzaal <100206484+ShibCode@users.noreply.github.com>
2022-07-28 15:17:47 +0500

    commit

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,54 @@
+import React from "react";
+
+import { Link } from "react-router-dom";
+
+import Attractions from "../../assets/Attractions.png";
+import Hotels from "../../assets/Hotels.png";
+import Restaurants from "../../assets/Restaurants.png";
+
+import "./Home.css";
+
+const Home = () => {
+  return (
+    <div className="home">
+      <div className="home__headerContent">
+        <div className="home__logo">Travel Companion</div>
+
+        <div className="home__links">
+          <Link to="/explore" className="home__link">
+            Explore
+          </Link>
+          <div className="home__link">Contact Us</div>
+        </div>
+      </div>
+
+      <div className="home__body">
+        <div className="home__bodyLeft">
+          <h1 className="home__heading">Its time to explore the world!</h1>
+
+          <p className="home__para">
+            You will be able to choose the best restaurants, Hotels and
+            attractions according to your preferred location with the help of
+            our travel advisory
+          </p>
+        </div>
+
+        <div className="home__bodyRight">
+          <div>
+            <img src={Restaurants} alt="restaurants" />
+          </div>
+
+          <div>
+            <img src={Hotels} alt="hotel" />
+          </div>
+
+          <div>
+            <img src={Attractions} alt="attractions" />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
